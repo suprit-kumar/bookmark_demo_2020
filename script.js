b74e870ce0438e087f0003f995098a9d28902fba
Supritkumar <supritk16@gmail.com>
2020-10-02 16:44:50 +0530

    bookmark form validation process completed

--- script.js
+++ script.js
@@ -20,3 +20,42 @@ function showModal(){
 modalShow.addEventListener('click',showModal);
 modalClose.addEventListener('click',() => modal.classList.remove('show-modal'));
 window.addEventListener('click',(e) => (e.target === modal ? modal.classList.remove('show-modal'):false));
+
+// validate form
+function validate(nameValue,urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+
+    if(!nameValue || !urlValue){
+        alert('Please submit values for both fields.');
+        return false;
+    }
+
+    const regex = new RegExp(expression);
+  
+    if(!urlValue.match(regex)){
+        alert('Please provide valid web address');
+        return false;
+    }
+
+    return true;
+
+}
+
+
+// handle data from form
+function storeBookmark(e){
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://','https://')){
+        urlValue = `https://${urlValue}`;
+    }
+
+    if(!validate(nameValue,urlValue)){
+        return false;
+    }
+}
+
+
+// Event listners
+bookmarkForm.addEventListener('submit',storeBookmark);
