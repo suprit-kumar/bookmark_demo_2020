4f7863b72924d651de2632498e1780c13aedd014
Supritkumar <supritk16@gmail.com>
2020-10-04 13:29:59 +0530

    new bookmarks added dynmaically to the bookamark container through javascript

--- script.js
+++ script.js
@@ -42,6 +42,37 @@ function validate(nameValue,urlValue){
 
 }
 
+// Build bookmarks DOM
+function buildBookmarks(){
+    bookmarks.forEach((bookmark)=>{
+        const {name , url } = bookmark;
+    //    Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas','fa-times');
+    closeIcon.setAttribute('title','Delete Bookmark');
+    closeIcon.setAttribute('onclick',`deleteBookmark('${url}')`);
+    // Favicon  / Link container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    const favicon = document.createElement('img');
+    favicon.setAttribute('src',`https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+    favicon.setAttribute('alt','Favicon');
+    const link  = document.createElement('a');
+    link.setAttribute('href',`${url}`);
+    link.setAttribute('target','_blank');
+    link.textContent = name;
+    // Append to bookmark conatiner
+
+    linkInfo.append(favicon,link);
+    item.append(closeIcon,linkInfo);
+    bookmarksContainer.appendChild(item);
+    });
+}
+
+
+
 // Fetch bookmarks from local storage if available
 
 function fetchBookmarks(){
@@ -58,7 +89,7 @@ function fetchBookmarks(){
         ];
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
     }
-    console.log(bookmarks);
+    buildBookmarks();
 }
 
 
